ae21bed938671d5902790507ce72171c55760260
llacko6723 <lackoluka1@gmail.com>
2026-04-02 23:54:29 +0200

    seeder: add workingHour seeder data

--- database/seeders/BarberSeeder.php
+++ database/seeders/BarberSeeder.php
@@ -18,14 +18,14 @@ class BarberSeeder extends Seeder
         $barber->name = 'Miloš Milosević';
         $barber->bio = "Miloš je završio frizersku školu i ima puno iskustva jer celog svog života se bavi ovim poslom.";
         $barber->photo = '';
-        $barber->is_available = 1;
+        $barber->is_available = true;
 
         $barber = new Barber();
         $barber->user_id = 2;
         $barber->name = 'Ana Panković';
         $barber->bio = "Ana je završila frizersku obuku u Londonu i ima puno iskustva jer celog svog života se bavi ovim poslom.";
         $barber->photo = '';
-        $barber->is_available = 1;
+        $barber->is_available = true;
 
     }
 }
--- database/seeders/WorkingHourSeeder.php
+++ database/seeders/WorkingHourSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\WorkingHour;
 
 class WorkingHourSeeder extends Seeder
 {
@@ -12,6 +13,37 @@ class WorkingHourSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $days = [
+            ['day' => 0, 'off' => true, 'start' => null, 'end' => null],
+            ['day' => 1, 'off' => false, 'start' => '09:00', 'end' => '17:00'],
+            ['day' => 2, 'off' => false, 'start' => '09:00', 'end' => '17:00'],
+            ['day' => 3, 'off' => false, 'start' => '09:00', 'end' => '17:00'],
+            ['day' => 4, 'off' => false, 'start' => '09:00', 'end' => '17:00'],
+            ['day' => 5, 'off' => false, 'start' => '09:00', 'end' => '17:00'],
+            ['day' => 6, 'off' => true, 'start' => null, 'end' => null],
+        ];
+
+        foreach($days as $day)
+        {
+            WorkingHour::create([
+                'barber_id' => 1,
+                'day_of_the_week' => $day['day'],
+                'start_time' => $day['start'],
+                'end_time' => $day['end'],
+                'is_day_off' => $day['off']
+            ]);
+        }
+
+        foreach($days as $day)
+        {
+            WorkingHour::create([
+                'barber_id' => 2,
+                'day_of_the_week' => $day['day'],
+                'start_time' => $day['start'],
+                'end_time' => $day['end'],
+                'is_day_off' => $day['off']
+            ]);
+        }
+
     }
 }
