6145e3067a1058013c47c6e65487e2fe4698bebf
llacko6723 <lackoluka1@gmail.com>
2026-04-03 15:03:06 +0200

    routes: add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashbaordController;
+use App\Http\Controllers\Barber\DashboardController as BarberDashbaordController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function(){
+    Route::get('/dashboard', [AdminDashbaordController::class, 'index'])->name('dashbaord');
+});
+
+Route::middleware(['auth', 'barber'])->prefix('barber')->name('barber.')->group(function(){
+    Route::get('/dashboard', [BarberDashbaordController::class, 'index'])->name('dashboard');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
