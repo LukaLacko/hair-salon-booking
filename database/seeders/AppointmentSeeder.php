611b752a83be3db1d020be0cb8bf5434f26fec85
llacko6723 <lackoluka1@gmail.com>
2026-04-03 12:28:10 +0200

    seeder: add appointment and client seeder data

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Appointment;
 
 class AppointmentSeeder extends Seeder
 {
@@ -12,6 +13,44 @@ class AppointmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $appointment = new Appointment();
+        $appointment->barber_id = 1;
+        $appointment->client_id = 1;
+        $appointment->service_id = 1;
+        $appointment->start_time = '2026-04-01 14:00:00';
+        $appointment->end_time = '2026-04-01 14:30:00';
+        $appointment->status = 'Zavšeno';
+        $appointment->notes = null;
+        $appointment->price = 1000;
+
+        $appointment = new Appointment();
+        $appointment->barber_id = 2;
+        $appointment->client_id = 2;
+        $appointment->service_id = 5;
+        $appointment->start_time = '2026-04-02 14:00:00';
+        $appointment->end_time = '2026-04-02 13:30:00';
+        $appointment->status = 'Zavšeno';
+        $appointment->notes = 'Klijent želi da ostane ista dužina kose.';
+        $appointment->price = 1500;
+
+        $appointment = new Appointment();
+        $appointment->barber_id = 2;
+        $appointment->client_id = 3;
+        $appointment->service_id = 1;
+        $appointment->start_time = '2026-04-15 09:30:00';
+        $appointment->end_time = '2026-04-15 10:30:00';
+        $appointment->status = 'Na čekanju';
+        $appointment->notes = null;
+        $appointment->price = 1200;
+
+        $appointment = new Appointment();
+        $appointment->barber_id = 2;
+        $appointment->client_id = 4;
+        $appointment->service_id = 1;
+        $appointment->start_time = '2026-04-10 15:30:00';
+        $appointment->end_time = '2026-04-10 16:00:00';
+        $appointment->status = 'Potvrđeno';
+        $appointment->notes = null;
+        $appointment->price = 1200;
     }
 }
--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -13,6 +13,29 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
+        $client = new Client();
+        $client->name = 'Nikolina Tarzić';
+        $client->email = 'nikolina@gmail.com';
+        $client->phone= '063142312';
+        $client->notes= 'Voli kratku kosu i ima mačku ljubimca koji se zove Arnold';
+
+        $client = new Client();
+        $client->name = 'Milan Mitrije';
+        $client->email = 'milan@gmail.com';
+        $client->phone= '061423161';
+        $client->notes= 'Ima dva brata i sestru';
+
+        $client = new Client();
+        $client->name = 'Nada Demiter';
+        $client->email = 'nada@gmail.com';
+        $client->phone= '063142312';
+        $client->notes= 'Presla pre kod nas na šišanje zbog lošeg iskustva sa frizerima u drugoj firmi!';
+        
+        $client = new Client();
+        $client->name = 'Bane Lazerovac';
+        $client->email = 'bane@gmail.com';
+        $client->phone= '06465219';
+        $client->notes= '';
         
     }
 }
